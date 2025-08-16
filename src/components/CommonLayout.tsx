395d535d40940331110e6aa27893d4b255a61d58
hara.jun <jh.dev.acc.1026.i2k9@gmail.com>
2025-08-16 10:55:40 +0900

    feat: arrange CommonLayout

--- src/components/CommonLayout.tsx
+++ src/components/CommonLayout.tsx
@@ -1,14 +1,23 @@
 import { Outlet } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "./ui/sidebar";
 import { AppSidebar } from "./AppSideber";
+import { cn } from "@/lib/utils";
 
 const CommonLayout = () => {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
+      <main className={cn("w-full flex")}>
         <SidebarTrigger />
-        <Outlet />
+        <div className={cn("w-[calc(100%-260px)] flex justify-center")}>
+          <div
+            className={cn(
+              "min-w-[1024px] w-[1024px] h-[calc(100vh)] bg-amber-100"
+            )}
+          >
+            <Outlet />
+          </div>
+        </div>
       </main>
     </SidebarProvider>
   );
